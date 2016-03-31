c5b57ed5b59c614954376d3b13162b939ef1cb3e
stcalica <kycalica@gmail.com>
2016-03-31 09:35:11 -0700

    I believe I have it running correctly

--- tasks.py
+++ tasks.py
@@ -1,11 +1,8 @@
 from celery import Celery as c
 
 
-app = c('tasks', broker='amqp//rabbit:5432')
+app = c('tasks', backend="amqp", broker='localhost')
 
-
-@app.task
-def add():
-   a = 0
-   b = a + 1
-   return 
+@app.task 
+def hello():
+ print 'hello there'
